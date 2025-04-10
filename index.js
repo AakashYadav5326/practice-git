a2f5947ad9765c3fa621224f9bfa034bc2c3fef6
aakash yadav <contact.aakash1504@gmail.com>
2025-04-10 16:18:53 +0530

    subract function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+//first commit 
 console.log('hello world');
 
  var a =10; var b =20;
-
+//2.sum function commit
  function sum (a,b){
     return a+b;
  }
 console.log(sum(a,b));
+//3.subract function commit
+function subract(a,b){
+    return a-b;
+}
  
+console.log(subract(a,b));
  
